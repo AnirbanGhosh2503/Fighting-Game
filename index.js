7c281012a69cefe0efc1df1734a37b019ece15ea
Anirban Ghosh <2003anirbanghosh@gmail.com>
2024-12-01 00:15:55 +0530

    Implemented Health Bar Interface

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -165,7 +166,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false
-        console.log('go')
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if(
@@ -176,7 +178,8 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false
-        console.log('enemy attack successful')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
@@ -196,6 +199,7 @@ window.addEventListener('keydown', (event) => {
             player.velocity.y = -20
             break
         case ' ':
+            event.preventDefault();
             player.attack()
             break
         case 'ArrowRight':
